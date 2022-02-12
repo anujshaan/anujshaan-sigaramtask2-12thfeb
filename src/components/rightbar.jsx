da445e449cabc98300c7b22083b5b710fdc4435c
Anuj Shaan <anujprakashshaan@gmail.com>
2022-02-12 12:38:28 +0530

    rightbar

--- src/components/rightbar.jsx
+++ src/components/rightbar.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Rightbar = () => {
   return (
-    <div className='rightbarWrapper'>Rightbar</div>
+    <div className='rightbarWrapper'>
+      <div className='rightbarHeader'>
+        <div style={{display:'flex'}}>
+          <p>My Card</p>
+          <small>clear cart</small>
+        </div>
+        <hr />
+      </div>
+      <div>
+
+      </div>
+      <div className='cartValue'>
+        <hr />
+        <p>Items Total</p>
+        <p>$200</p>
+      <button className='checkoutButton'>Check Out</button>
+      </div>
+    </div>
   )
 }
 
